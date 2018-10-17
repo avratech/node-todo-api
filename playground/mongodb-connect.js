f02186d6d85c185d33e3f9599b1e02641f430265
Aravind A <aa.avratech@outlook.com>
2018-10-17 16:46:35 -0500

    added find, delete

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -8,6 +8,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true 
     console.log('Successfully connected to Mongdb server');
     const db = client.db('TodoApp');
 
+///////////////////////////////////////Adding records
+
     // db.collection('Todos').insertOne({
     //     name: 'Vyj',
     //     age: 34,
@@ -31,7 +33,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true 
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    //// Finding/fetching records from DB - all and based on Query
+///////////////////////////////////////Finding/fetching records from DB - all and based on Query
 
     // db.collection('Users').find().toArray().then((result) => {
     //     console.log(JSON.stringify(result, undefined, 2));
@@ -57,12 +59,38 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true 
     //     return console.log('Unable to add user', err);
     // });
 
-    db.collection('Users').find({adult: true, _id: new ObjectID('5bc794d454d214e9cc5581ab')}).toArray().then((result) => {
-        // console.log(`There are ${result} adult users in total`);
-        console.log(JSON.stringify(result, undefined, 2));
+    // db.collection('Users').find({adult: true, _id: new ObjectID('5bc794d454d214e9cc5581ab')}).toArray().then((result) => {
+    //     // console.log(`There are ${result} adult users in total`);
+    //     console.log(JSON.stringify(result, undefined, 2));
+    // }, (err) => {
+    //     return console.log('Unable to add user', err);
+    // });
+
+/////////////////////////////////////// Deleting records
+
+    //Delete one
+    //     db.collection('Todos').deleteOne({todo : "Obtain OCP for Java"}).then((res) => {
+    //         console.log(`Deleting the first occurrence of the criteria provided, total records deleted: ${res.n}`);
+    //     });
+
+
+    //Delete Many
+    // db.collection('Todos').deleteMany({todo: "Pick up kids"}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // find one and Delete
+    // db.collection('Todos').findOneAndDelete({todo : "Pick up kids", completed: true}).then((res) => {
+    //    console.log(res);
+    // });
+
+    // find & delete by ObjectID
+    db.collection('Todos').findOneAndDelete({_id: new ObjectID('5bc7a80bb57cedab96afd098')}).then((res) => {
+        console.log(res);
     }, (err) => {
-        return console.log('Unable to add user', err);
+        console.log('Error while deleteing', err);
     });
+/////////////////////////////////////// Deleting records
 
     // client.close();
 
